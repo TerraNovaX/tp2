75c8b55ed7987e4e1bbf5caf258c345d6404c4b7
Salma WADOUACHI <wadouachisalma@gmail.com>
2025-07-23 10:37:58 +0200

    fix: main.tf

--- Infra/main.tf
+++ Infra/main.tf
@@ -1,7 +1,11 @@
+resource "random_integer" "random" {
+  min = 1
+  max = 50000
+}
+
 resource "aws_s3_bucket" "main" {
   bucket = "${var.bucket_name}-${random_integer.random.result}"
-
-  tags = var.tags
+  tags   = var.tags
 }
 
 resource "aws_s3_bucket_website_configuration" "main" {
@@ -18,6 +22,7 @@ resource "aws_s3_bucket_website_configuration" "main" {
 
 resource "aws_s3_bucket_ownership_controls" "main" {
   bucket = aws_s3_bucket.main.id
+
   rule {
     object_ownership = "BucketOwnerPreferred"
   }
@@ -32,63 +37,55 @@ resource "aws_s3_bucket_public_access_block" "main" {
   restrict_public_buckets = false
 }
 
-resource "aws_s3_bucket_policy" "allow_content_public" {
-  bucket = aws_s3_bucket.main.id
-  policy = data.aws_iam_policy_document.allow_content_public.json
-
-  depends_on = [
-    aws_s3_bucket_public_access_block.main
-  ]
-}
-
 data "aws_iam_policy_document" "allow_content_public" {
   statement {
     principals {
       type        = "*"
       identifiers = ["*"]
     }
+
     actions = [
       "s3:GetObject"
     ]
+
     resources = [
       "${aws_s3_bucket.main.arn}/*",
     ]
   }
 }
 
+resource "aws_s3_bucket_policy" "allow_content_public" {
+  bucket = aws_s3_bucket.main.id
+  policy = data.aws_iam_policy_document.allow_content_public.json
+}
+
 resource "aws_s3_object" "sync_remote_website_content" {
   for_each = fileset(var.sync_directories[0].local_source_directory, "**/*.*")
 
-  bucket = aws_s3_bucket.main.id
-  key    = "${var.sync_directories[0].s3_target_directory}/${each.value}"
-  source = "${var.sync_directories[0].local_source_directory}/${each.value}"
-  etag   = filemd5("${var.sync_directories[0].local_source_directory}/${each.value}")
+  bucket       = aws_s3_bucket.main.id
+  key          = "${var.sync_directories[0].s3_target_directory}/${each.value}"
+  source       = "${var.sync_directories[0].local_source_directory}/${each.value}"
+  etag         = filemd5("${var.sync_directories[0].local_source_directory}/${each.value}")
   content_type = try(
     lookup(var.mime_types, split(".", each.value)[length(split(".", each.value)) - 1]),
     "binary/octet-stream"
   )
 }
 
-resource "random_integer" "random" {
-  min = 1
-  max = 50000
-}
-
 resource "aws_cloudfront_origin_access_control" "s3_access" {
-  name                              = "s3-oac-${random_integer.random.result}"
-  description                       = "OAC for static site"
-  origin_access_control_origin_type = "s3"
-  signing_behavior                  = "always"
-  signing_protocol                  = "sigv4"
+  name                               = "s3-oac-${random_integer.random.result}"
+  description                        = "OAC for static site"
+  origin_access_control_origin_type  = "s3"
+  signing_behavior                   = "always"
+  signing_protocol                   = "sigv4"
 }
 
 resource "aws_cloudfront_distribution" "cdn" {
   enabled = true
 
   origin {
-    domain_name = aws_s3_bucket.main.bucket_regional_domain_name
-    origin_id   = "S3Origin"
-
+    domain_name             = aws_s3_bucket.main.bucket_regional_domain_name
+    origin_id               = "S3Origin"
     origin_access_control_id = aws_cloudfront_origin_access_control.s3_access.id
   }
 
@@ -120,9 +117,4 @@ resource "aws_cloudfront_distribution" "cdn" {
   }
 
   tags = var.tags
-}
-
-output "cloudfront_domain_name" {
-  description = "URL de la distribution CloudFront"
-  value       = aws_cloudfront_distribution.cdn.domain_name
 }
